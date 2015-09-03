12c524786242325ac0c34945f400c6459422c3e7
Read Sprabery <read.sprabery@gmail.com>
2015-09-03 08:18:51 -0500

    Using TreeMultiMap for LRU cache

--- src/org/hdfscache/idecider/LRUCache.java
+++ src/org/hdfscache/idecider/LRUCache.java
@@ -1,13 +1,19 @@
 package org.hdfscache.idecider;
 
-import java.util.TreeMap;
+import java.util.Comparator;
+import java.util.NavigableSet;
+
+import com.google.common.collect.Multimaps;
+import com.google.common.collect.SortedSetMultimap;
+import com.google.common.collect.TreeMultimap;
 
 /**
  * Created by read on 8/26/15.
  */
 public class LRUCache implements Cache {
 
-    private final TreeMap<Long, Inode> lruHash = new TreeMap<Long, Inode>();
+    // key is the access time, value is an Inode or list of Inodes at that acess time
+    private final TreeMultimap<Long, Inode> lruHash = TreeMultimap.create(new LongComparator(), new InodeComparator());
     private int CACHE_SIZE = 500;
 
     @Override
@@ -32,7 +38,6 @@ public class LRUCache implements Cache {
         }
     }
 
-
     /*
     This check is the definition of an LRU cache, but isn't really needed. The fact this is getting called means that
     it has been accessed more recently than the smallest value in the tree hash.
@@ -42,7 +47,7 @@ public class LRUCache implements Cache {
         if (lruHash.size() < CACHE_SIZE) {
             return true;
         } else {
-            return file.getAccesstime() > lruHash.firstKey();
+            return file.getAccesstime() > lruHash.keySet().first();
         }
     }
 
@@ -52,13 +57,34 @@ public class LRUCache implements Cache {
     We then add the inode "file" passed in to the cache and mark it as cached.
      */
     public void addToCache(Inode file) {
+
         if (!(lruHash.size() < CACHE_SIZE)) {
             // if it's full, remove the least recently used file and then insert our new one.
-            lruHash.firstEntry().getValue().setCached(false);
-            lruHash.remove(lruHash.firstKey());
+            // if there are multiple elements with the same access time, we'll grab the one
+            // with the lowest id.
+            Inode oldEntry = lruHash.get(lruHash.keySet().first()).first();
+            synchronized (oldEntry) {
+                oldEntry.setCached(false);
+                lruHash.remove(lruHash.keySet().first(), oldEntry);
+            }
         }
+
         lruHash.put(file.getAccesstime(), file);
         file.setCached(true);
     }
 
+}
+
+class InodeComparator implements Comparator<Inode> {
+    @Override
+    public int compare(Inode o1, Inode o2) {
+        return (int)(o1.getInodeId() - o2.getInodeId());
+    }
+}
+
+class LongComparator implements Comparator<Long> {
+    @Override
+    public int compare(Long o1, Long o2) {
+        return o1.intValue() - o2.intValue();
+    }
 }
